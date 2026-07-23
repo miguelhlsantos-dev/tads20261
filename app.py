ce25d70121b10bc1d227a957e8f2c3ecd608a59d
Miguel Santos <miguelluzhlds@gmail.com>
2026-07-23 22:50:43 +0000

    feat: ma strategy and add tabs

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
 import streamlit as st
 from functions.plot import plot_history
+from functions.backtest import ma_strategy
 
-st.title("Stocks History")
-st.write("Look the stock values.")
+tab1, tab2 = st.tabs([
+    'History', 'MA Strategy'
+])
 
 ticker = st.sidebar.text_input(
-    'Choose the ticket:',
+    'Choose the ticker:',
     value = 'NVDA'
 )
 
-fig = plot_history(ticker)
-st.plotly_chart(fig)
+ma_short_window = st.sidebar.number_input(
+    "MA Short",
+    min_value = 1,
+    max_value = 100,
+    value = 9,
+    step = 1
+)
+
+ma_long_window = st.sidebar.number_input(
+    "MA Long",
+    min_value = 1,
+    max_value = 300,
+    value = 72,
+    step = 1
+)
+
+
+with tab1:
+    st.title('Stocks History')
+    st.write('Look the stock values.')
+
+    fig = plot_history(ticker)
+    st.plotly_chart(fig)
+
+with tab2:
+    st.title('MA Strategy')
+    st.write('Look your portfolio.')
+
+    df = ma_strategy(
+        ticker = ticker,
+        ma_short = ma_short_window,
+        ma_long = ma_long_window
+    )
+
+    st.dataframe(df)
